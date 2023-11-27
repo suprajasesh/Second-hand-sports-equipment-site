662a9fe576b5c5a1fa390fd59863b0393ed0c9ce
Supraja Seshadri <112061236+suprajasesh@users.noreply.github.com>
2023-11-27 21:20:34 +0530

    Add files via upload

---
+++ item_schema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+let itemSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    item: String,
+    yold: Number,
+    price: Number,
+    desc: String,
+    sold: Boolean
+});
+
+let item = module.exports = mongoose.model('item', itemSchema);
